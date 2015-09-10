f9f62a18e6d2515d58751e31e3e4354346f7f52a
Arnaud Wetzel <arnaud.wetzel@kbrwadventure.com>
2015-09-10 09:11:57 +0200

    bump to version 0.0.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PlugForwardedPeer.Mixfile do
 
   def project do
     [app: :plug_forwarded_peer,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
