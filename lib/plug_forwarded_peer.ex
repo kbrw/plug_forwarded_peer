ffa7324a189417ca5190c21955e97b8e4e6c2b34
Kiere El-Shafie <kiere@mac.com>
2019-02-03 21:46:24 -0500

    Update and fix deprecations in lib file

--- lib/plug_forwarded_peer.ex
+++ lib/plug_forwarded_peer.ex
@@ -1,31 +1,52 @@
 defmodule PlugForwardedPeer do
   import Plug.Conn
+
   def init(_), do: []
-  def call(conn,_) do
-    case get_req_header(conn,"x-forwarded-for") do
-      []->
-        case get_req_header(conn,"forwarded") do
-          []-> conn
-          [header|_]->
-            ips = for "for="<>quoted_ip<-String.split(header,~r/\s*,\s*/), ip=clean_ip(quoted_ip), !is_nil(ip), do: ip
-            case ips do
-              []->conn
-              [ip|_]->%{conn|remote_ip: ip}
-            end
-        end
-      [header|_]->
-        ips = for quoted_ip<-String.split(header,~r/\s*,\s*/), ip=clean_ip(quoted_ip), !is_nil(ip), do: ip
-        case ips do
-          []->conn
-          [ip|_]->%{conn|remote_ip: ip}
-        end
+
+  def call(conn, _) do
+    cond do
+      header_data = extract_data_from_header(conn, "x-forwarded-for") ->
+        extract_and_replace_remote_ip(conn, header_data)
+
+      header_data = extract_data_from_header(conn, "forwarded") ->
+        extract_and_replace_remote_ip(conn, header_data)
+
+      true ->
+        conn
     end
   end
-  def clean_ip(maybe_quoted_ip) do
-    maybe_ip = maybe_quoted_ip |> String.strip(?") |> String.rstrip(?]) |> String.lstrip(?[)
+
+  defp clean_ip(maybe_quoted_ip) do
+    maybe_ip =
+      maybe_quoted_ip
+      |> String.trim(~s(for=))
+      |> String.trim(~s("))
+      |> String.trim_trailing("]")
+      |> String.trim_leading("[")
+
     case :inet_parse.address('#{maybe_ip}') do
-      {:ok,ip}->ip
-      _->nil
+      {:ok, ip} -> ip
+      _ -> nil
+    end
+  end
+
+  defp extract_data_from_header(conn, header_name) do
+    case get_req_header(conn, header_name) do
+      [] -> nil
+      [head | _tail] -> head
+    end
+  end
+
+  defp extract_and_replace_remote_ip(conn, data) do
+    ips =
+      data
+      |> String.split(~r/\s*,\s*/)
+      |> Enum.map(&clean_ip/1)
+      |> Enum.reject(&is_nil/1)
+
+    case ips do
+      [] -> conn
+      [ip | _] -> %{conn | remote_ip: ip}
     end
   end
 end
