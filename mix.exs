6bcb186ae02278c78779769cb6c264a5ed5cb3db
Kiere El-Shafie <kiere@mac.com>
2019-02-03 21:52:38 -0500

    Bump version and contributors

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PlugForwardedPeer.Mixfile do
 
   def project do
     [app: :plug_forwarded_peer,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -12,7 +12,7 @@ defmodule PlugForwardedPeer.Mixfile do
      to rfc7239 and fill `conn.remote_ip` with the root client ip.
      """,
      package: [links: %{"Source"=>"http://github.com/awetzel/plug_forwarded_peer"},
-               contributors: ["Arnaud Wetzel"],
+               contributors: ["Arnaud Wetzel", "Kiere El-Shafie"],
                licenses: ["MIT"],
                files: ["lib", "priv", "mix.exs", "README*", "templates", "LICENSE*"]],
      deps: deps()]
